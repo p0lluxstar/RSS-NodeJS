5b6e517a362061e21ea1d6d386d5b90d7bf1442c
p0lluxstar <web-registra@yandex.ru>
2024-02-10 15:26:37 +0300

    fear: add bd

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import http from 'http';
+import { users } from './db/users';
 import dotenv from 'dotenv';
 dotenv.config();
 
 const port: string = process.env.PORT || '';
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello world!');
+  if (req.url === 'api/users' && req.method === 'GET') {
+    res.writeHead(200, { 'Content-type': 'application/json' });
+  }
+  res.end(JSON.stringify(users));
 });
 
 server.listen(Number(port), 'localhost', () => {
