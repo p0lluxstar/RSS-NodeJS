7098326715bf1bc8857886660697c386be586102
p0lluxstar <web-registra@yandex.ru>
2024-02-10 18:55:30 +0300

    feat: id user from uuid

--- src/util/handler.ts
+++ src/util/handler.ts
@@ -1,4 +1,5 @@
 import { IncomingMessage, ServerResponse } from 'http';
+import { v4 as uuidv4 } from 'uuid';
 import { users } from '../db/users';
 import { User } from '../interface/users';
 
@@ -8,7 +9,8 @@ export const handleGetUsers = (res: ServerResponse) => {
 };
 
 export const handleGetUserById = (res: ServerResponse, userId: string) => {
-  if (userId.length < 3) {
+  const lengthID = 36;
+  if (userId.length != lengthID) {
     res.writeHead(400, { 'Content-type': 'application/json' });
     res.end(JSON.stringify({ code: 400, message: 'User in invalid' }));
   } else {
@@ -27,16 +29,17 @@ export const handlePostUser = (req: IncomingMessage, res: ServerResponse) => {
   let userJSON = '';
   req.on('data', (chunk: string) => (userJSON += chunk));
   req.on('end', () => {
-    const userData: User = JSON.parse(userJSON);
-    console.log('userData', userData);
-    if(userData.username && userData.age && userData.hobbies.length > 0) {
-      users.push(userData);
+    const userDataWithoutID: User = JSON.parse(userJSON);
+    if (userDataWithoutID.username && userDataWithoutID.age && userDataWithoutID.hobbies.length > 0) {
+      const id = uuidv4();
+      const userDataWithID = {id, ...userDataWithoutID}
+      users.push(userDataWithID);
       res.writeHead(200, { 'Content-type': 'application/json' });
-      res.end(JSON.stringify({ message: 'User added successfully' }));
+      res.end(JSON.stringify(userDataWithID ));
     } else {
       res.writeHead(400, { 'Content-type': 'application/json' });
-      res.end(JSON.stringify({code: 400, message: 'Invalid user data' }));
-    } 
+      res.end(JSON.stringify({ code: 400, message: 'Invalid user data' }));
+    }
   });
 };
 
