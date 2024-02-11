f86c03576c45cffc6584d38bf26bb06cb724c807
p0lluxstar <web-registra@yandex.ru>
2024-02-11 20:11:47 +0300

    fix: handlers

--- src/util/handlers.ts
+++ src/util/handlers.ts
@@ -5,21 +5,27 @@ import { handlerResAnswer } from './handlerResAnswer';
 
 const LENGTH_ID = 36;
 
+const handleUserRequest = (res: ServerResponse, userId: string, callback: (user: any) => void) => {
+  if (userId.length !== LENGTH_ID) {
+    handlerResAnswer(res, 400, { code: 400, message: 'User in invalid' });
+  } else {
+    const user = users.find((user) => user.id === userId);
+    if (user) {
+      callback(user);
+    } else {
+      handlerResAnswer(res, 404, { code: 404, message: 'User not found' });
+    }
+  }
+};
+
 export const handleGetUsers = (res: ServerResponse) => {
   handlerResAnswer(res, 200, users);
 };
 
 export const handleGetUserById = (res: ServerResponse, userId: string) => {
-  if (userId.length != LENGTH_ID) {
-    handlerResAnswer(res, 400, { code: 400, message: 'User in invalid' });
-  } else {
-    const user = users.find((user) => user.id === userId);
-    if (user) {
-      handlerResAnswer(res, 200, user);
-    } else {
-      handlerResAnswer(res, 404, { code: 404, message: 'User not found' });
-    }
-  }
+  handleUserRequest(res, userId, (user: any) => {
+    handlerResAnswer(res, 200, user);
+  });
 };
 
 export const handlePostUser = (req: IncomingMessage, res: ServerResponse) => {
@@ -39,36 +45,22 @@ export const handlePostUser = (req: IncomingMessage, res: ServerResponse) => {
 };
 
 export const handlePutUser = (req: IncomingMessage, res: ServerResponse, userId: string) => {
-  if (userId.length != LENGTH_ID) {
-    handlerResAnswer(res, 400, { code: 400, message: 'User in invalid' });
-  } else {
-    const user = users.find((user) => user.id === userId);
-    if (user) {
-      let putUserJSON = '';
-      req.on('data', (chunk: string) => (putUserJSON += chunk));
-      req.on('end', () => {
-        const newDataUser = JSON.parse(putUserJSON);
-        Object.assign(user, newDataUser);
-        handlerResAnswer(res, 200, user);
-      });
-    } else {
-      handlerResAnswer(res, 404, { code: 404, message: 'User not found' });
-    }
-  }
+  handleUserRequest(res, userId, (user: any) => {
+    let putUserJSON = '';
+    req.on('data', (chunk: string) => (putUserJSON += chunk));
+    req.on('end', () => {
+      const newDataUser = JSON.parse(putUserJSON);
+      Object.assign(user, newDataUser);
+      handlerResAnswer(res, 200, user);
+    });
+  });
 };
 
 export const handleDeleteUser = (req: IncomingMessage, res: ServerResponse, userId: string) => {
-  if (userId.length != LENGTH_ID) {
-    handlerResAnswer(res, 400, { code: 400, message: 'User in invalid' });
-  } else {
-    const user = users.find((user) => user.id === userId);
-    if (user) {
-      deleteUsers(userId);
-      handlerResAnswer(res, 204, {});
-    } else {
-      handlerResAnswer(res, 404, { code: 404, message: 'User not found' });
-    }
-  }
+  handleUserRequest(res, userId, (user: any) => {
+    deleteUsers(userId);
+    handlerResAnswer(res, 204, {});
+  });
 };
 
 export const pageNotFound = (res: ServerResponse) => {
