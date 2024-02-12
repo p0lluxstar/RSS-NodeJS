7cd0f1552d25d6f1b640e3732d99d5cbf4a69982
p0lluxstar <web-registra@yandex.ru>
2024-02-12 12:39:11 +0300

    fix: tests

--- src/tests/api.test.ts
+++ src/tests/api.test.ts
@@ -1,15 +1,15 @@
-const http = require('http');
-const request = require('supertest');
-const { handleGetUsers, handleGetUserById } = require('../util/handlers');
+import http from 'http';
+import supertest from 'supertest';
+import { handleGetUsers, handleGetUserById } from '../util/handlers';
+import { IncomingMessage, ServerResponse } from 'http';
 
 const port = 3000;
 
 describe('GET /api/users', () => {
-
-  let server;
+  let server: any;
 
   beforeAll(() => {
-    server = http.createServer((req, res) => {
+    server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
       if ((req.url === '/api/users' || req.url === '/api/users/') && req.method === 'GET') {
         handleGetUsers(res);
       }
@@ -26,19 +26,19 @@ describe('GET /api/users', () => {
   });
 
   test('should return status 200', async () => {
-    const response = await request(server).get('/api/users');
+    const response = await supertest(server).get('/api/users');
 
     expect(response.status).toBe(200);
   });
 
   test('should return an array of users', async () => {
-    const response = await request(server).get('/api/users');
+    const response = await supertest(server).get('/api/users');
 
     expect(Array.isArray(response.body)).toBe(true);
   });
 
   test('should return 400 - User in invalid', async () => {
-    const response = await request(server).get('/api/users/invalid-id-user');
+    const response = await supertest(server).get('/api/users/invalid-id-user');
     expect(response.status).toBe(400);
   });
 });
